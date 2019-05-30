8a2a170b45028199368f410febe3e8b378cfcb78
Caleb Scott <duckduckdoof@gmail.com>
2019-05-30 11:01:41 -0400

    Update i3-gaps-init.sh

--- i3-gaps-init.sh
+++ i3-gaps-init.sh
@@ -20,7 +20,7 @@ cd tmp
 git clone https://github.com/Airblader/xcb-util-xrm
 cd xcb-util-xrm
 git submodule update --init
-./autogen.sh --prefix==/usr
+./autogen.sh --prefix=/usr
 make
 sudo make install
 

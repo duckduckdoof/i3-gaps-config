8dd973dd7a31889374f09fc8a82507639b6e5c13
Caleb Scott <duckduckdoof@gmail.com>
2019-05-30 11:33:38 -0400

    Update i3-gaps-init.sh

--- i3-gaps-init.sh
+++ i3-gaps-init.sh
@@ -15,8 +15,7 @@ sudo apt install vim lxappearance x11-utils tree rofi compton git scrot feh rang
 sudo apt install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev xutils-dev libxcb-shape0-dev autoconf
 
 # Need to install the libxcb-xrm-dev library
-mkdir tmp
-cd tmp
+cd /tmp
 git clone https://github.com/Airblader/xcb-util-xrm
 cd xcb-util-xrm
 git submodule update --init
@@ -25,10 +24,9 @@ make
 sudo make install
 
 # Install from source
-cd tmp
+cd /tmp
 git clone https://github.com/Airblader/i3 i3-gaps
 cd i3-gaps
-git checkout gaps && git pull
 autoreconf --force --install
 rm -rf build
 mkdir build

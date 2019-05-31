d3fa1951eecb5b0580277a39799ffb5de315a5d4
Caleb Scott <duckduckdoof@gmail.com>
2019-05-30 20:28:11 -0400

    Update i3-gaps-init.sh

--- i3-gaps-init.sh
+++ i3-gaps-init.sh
@@ -99,4 +99,4 @@ chmod +x ~/.scripts/polybar.sh
 # Install google chrome
 cd ~/Downloads
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd54.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
